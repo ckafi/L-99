b5961bbcdb7f145f65f66c0cd65aad90383190a8
Tobias Frilling <tobias@frilling-online.de>
2014-11-24 20:53:38 +0100

    Remove recursion from P02

--- P02.clj
+++ P02.clj
@@ -1,6 +1,4 @@
 ; Find the last but one box of a list
 (defn my-but-last [coll]
-  (cond (zero? (count coll)) nil
-        (= 1 (count coll)) nil
-        (= 2 (count coll)) (first coll)
-        :else (recur (rest coll))))
+  (if (= 1 (count coll)) nil
+    (first (drop (- (count coll) 2) coll))))
