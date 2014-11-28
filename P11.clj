d2df0c01e64fba10e3752464eb23b2b076bbc878
Tobias Frilling <tobias@frilling-online.de>
2014-11-28 17:49:07 +0100

    Solution to P11

---
+++ P11.clj
@@ -0,0 +1,6 @@
+(defn encode [coll]
+  (map #(list (count %) (first %))
+       (partition-by identity coll)))
+
+(defn encode-modified [coll]
+  (map #(if (= 1 (first %)) (last %) %) (encode coll)))
