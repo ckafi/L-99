56332b83cebf2f2c93e9582896685657938f8ee4
Tobias Frilling <tobias@frilling-online.de>
2014-11-29 10:34:42 +0100

    Solution to P25

---
+++ P25.clj
@@ -0,0 +1,16 @@
+(defn remove-at [coll n]
+  (concat (take (dec n) coll) (drop n coll)))
+
+(defn rnd-select [coll n]
+  (loop [coll coll
+         n n
+         res []]
+    (if (zero? n)
+      res
+      (let [sel (rand-int (count coll))]
+        (recur (remove-at coll (inc sel))
+               (dec n)
+               (conj res (nth coll sel)))))))
+
+(defn rnd-permu [coll]
+  (rnd-select coll (count coll)))
