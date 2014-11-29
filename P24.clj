541bbc4940e6f91580c9d84d4ea7c454b04a9078
Tobias Frilling <tobias@frilling-online.de>
2014-11-29 10:31:52 +0100

    Solution to P24

---
+++ P24.clj
@@ -0,0 +1,19 @@
+(defn remove-at [coll n]
+  (concat (take (dec n) coll) (drop n coll)))
+
+(defn rnd-select [coll n]
+  (loop [coll coll
+         n n
+         res []]
+    (if (zero? n)
+      coll
+      (let [sel (rand-int (count coll))]
+        (recur (remove-at coll (inc sel))
+               (dec n)
+               (conj res (nth coll sel)))))))
+
+(defn my-range [start end]
+  (take (- end start) (iterate inc start)))
+
+(defn lotto-select [n m]
+  (rnd-select (my-range 1 (inc m)) n))
